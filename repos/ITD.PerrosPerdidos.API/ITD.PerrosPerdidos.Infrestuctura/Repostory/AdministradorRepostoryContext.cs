ba85f859753aada7bfed6da9c6b3be62a2a07399
AlexandraDVOG <145791983+AlexandraDVOG@users.noreply.github.com>
2024-03-20 10:07:55 -0600

    Add files via upload

---
+++ repos/ITD.PerrosPerdidos.API/ITD.PerrosPerdidos.Infrestuctura/Repostory/AdministradorRepostoryContext.cs
@@ -0,0 +1,31 @@
+﻿using ITD.PerrosPerdidos.Domain.DTO.DATA.Atributes;
+using ITD.PerrosPerdidos.Domain.POCOS.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITD.PerrosPerdidos.Infrestuctura.Repostory
+{
+    public class AdministradorRepostoryContext
+    {
+        public AdministradorRepostoryContext()
+        {
+
+        }
+        public list<AdministradorAtributes> GetAdministrador()
+        {
+            list<AdministradorContext> administrador new();
+            administrador.Add(new AdministradorContext {
+             
+                usuario="admin",
+                contraseña = "password",
+                telefono= 6181738095
+
+
+            }) ;
+            return administrador;
+        }
+    }
+}
