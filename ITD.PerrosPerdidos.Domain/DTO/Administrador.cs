4561229e663791f22d755f9776101f9a6f9629d6
AlexandraDVOG <145791983+AlexandraDVOG@users.noreply.github.com>
2024-03-20 18:51:18 -0600

    Add files via upload

---
+++ ITD.PerrosPerdidos.Domain/DTO/Administrador.cs
@@ -0,0 +1,10 @@
+﻿using ITD.PerrosPerdidos.Domain.DTO.DATA;
+namespace ITD.PerrosPerdidos.Domain.DTO
+{
+    public class Administrador
+    {
+        public AdministradorData data { get; set; }
+
+
+    }
+}
---
+++ ITD.PerrosPerdidos.Domain/DTO/DATA/Atributes/MascotasPerdidasAtributes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITD.PerrosPerdidos.Domain.DTO.DATA.Atributes
+{
+    public class MascotasPerdidasAtributes
+    {
+        public string raza { get; set; }
+        public string color { get; set; }
+        public string tamaño { get; set; }
+        public string sexo{ get; set; }
+        public string celular { get; set; }
+        public string imagen { get; set; }
+    }
+}
