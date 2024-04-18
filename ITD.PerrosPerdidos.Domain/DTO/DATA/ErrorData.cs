dcedd39a08a210b3a2b90248a43d2cf9356bbdcc
BridgetFraire <77119331+BridgetFraire@users.noreply.github.com>
2024-04-17 22:55:25 -0600

    Update ErrorData.cs

--- ITD.PerrosPerdidos.Domain/DTO/DATA/ErrorData.cs
+++ ITD.PerrosPerdidos.Domain/DTO/DATA/ErrorData.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ITD.PerrosPerdidos.Domain.DTO.DATA
+{
+  public class ErrorData
+  {
+    public string code {get;set;}
+    public string title {get;set;}
+    public string detail {get;set;}
+    public string status {get;set;}
+  }
+}
