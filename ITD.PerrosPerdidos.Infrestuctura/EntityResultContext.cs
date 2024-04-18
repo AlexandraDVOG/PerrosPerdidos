2bead7a27c31f114b509ca0fa6adad598c1e4732
BridgetFraire <77119331+BridgetFraire@users.noreply.github.com>
2024-04-17 22:27:35 -0600

    Update EntityResultContext.cs

--- ITD.PerrosPerdidos.Infrestuctura/EntityResultContext.cs
+++ ITD.PerrosPerdidos.Infrestuctura/EntityResultContext.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITD.PerrosPerdidos.Domain.POCOS.Context
+{
+  public class EntityResultConext
+  {
+    public int code {get;set;}
+    public string? result {get;set;}
+  }
+}
 
