a9a9beb69a880c0bc859ece13f9fe6b1b3b81220
BridgetFraire <77119331+BridgetFraire@users.noreply.github.com>
2024-04-17 22:34:17 -0600

    Create RequestPermisos.cs

---
+++ ITD.PerrosPerdidos.Domain/DTO/Requests/RequestPermisos.cs
@@ -0,0 +1,13 @@
+using System
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITD.PerrosPerdidos.Domain.DTO.Requests.Permisos
+{
+  public class RequestPermisos
+  {
+    public RequestPermisosData data {get;set;}
+  }
+}
