daf66a6f2c41c0e8f6dffbac9deb62d32e99d2db
BridgetFraire <77119331+BridgetFraire@users.noreply.github.com>
2024-04-17 22:52:24 -0600

    Update PermisosContext.cs

--- ITD.PerrosPerdidos.Infrestuctura/PermisosContext.cs
+++ ITD.PerrosPerdidos.Infrestuctura/PermisosContext.cs
@@ -7,11 +7,22 @@ namespace ITD.PerrosPerdidos.Infraestructura
 {
   public class PermisosContext
   {
+    public ErrorData ErrorData {get;set;}
     private readonly BdConext _bd;
     public PermisosContext(BdConext bd)
     {
       _bd = bd;
     }
-    public async Task<EntityResultConext>Post(PermisosPostData post)
+    public async Task<EntityResultConext>Post(RequestPermisos  post)
+    {
+      DynamicParameters dpr = new DynamicParameters();
+      dpr.Add("@usuario", post.data.usuario, System.Data.BdType.String, System.Data.ParameterDirection.Input);
+      dpr.Add("@niveles", post.data.niveles, System.Data.BdType.Boolean, System.Data.ParameterDirection.Input);
+      dpr.Add("@areas", post.data.areas, System.Data.BdType.Boolean, System.Data.ParameterDirection.Input);
+
+      var result await _bd.ExecuteStoredProcedureQueryFirstOrDefault<EntityResultContext>("Permisos_Post",dpr);
+      return result.code == 201 ? result : null;
+      
+    }
   }
 }
