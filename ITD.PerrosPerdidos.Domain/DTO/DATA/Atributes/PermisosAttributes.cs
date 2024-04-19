eff87e35248c2f3dd3d7f2363683e6dc26d80185
BridgetFraire <77119331+BridgetFraire@users.noreply.github.com>
2024-04-18 20:12:29 -0600

    Update PermisosAttributes.cs

--- ITD.PerrosPerdidos.Domain/DTO/DATA/Atributes/PermisosAttributes.cs
+++ ITD.PerrosPerdidos.Domain/DTO/DATA/Atributes/PermisosAttributes.cs
@@ -1,1 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITD.PerrosPerdidos.Domain.DTO.DATA.Attributes
+{
+  public class PermisosAttributes
+  {
+     public string mensaje {get;set;}
+    public PermisosAttributes attributes {get;set;}
+  }
+}
 
