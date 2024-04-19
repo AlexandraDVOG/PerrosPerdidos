36daf00a33c260c0e3d26a9dafb64dda1e08b347
BridgetFraire <77119331+BridgetFraire@users.noreply.github.com>
2024-04-18 20:26:24 -0600

    Create IPermisosPresenter.cs

---
+++ ITD.PerrosPerdidos.Aplication/Interfaces/Presenters/IPermisosPresenter.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITD.PerrosPerdidos.Aplication.Interfaces.Presenters
+{
+  public class IPermisosPresenter
+  {
+    public  ValueTask<PermisosResponse> Post(RequestPermisos post);
+  }
+
+}
