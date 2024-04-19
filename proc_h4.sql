3eea634486023b81cf24fb042f1403888e9650dd
BridgetFraire <77119331+BridgetFraire@users.noreply.github.com>
2024-04-19 11:07:02 -0600

    Update proc_h4.sql

--- proc_h4.sql
+++ proc_h4.sql
@@ -1,23 +1,22 @@
-CREATE OR REPLACE PROCEDURE reportar_perro_perdido(
-    p_id_usuario INT, -- Asumiendo que necesitamos este valor
-    p_celular INT, -- Asumiendo que necesitamos este valor
-    p_raza VARCHAR(50),
-    p_color VARCHAR(20),
-    p_tamano VARCHAR(20),
-    p_sexo CHAR(1),
-    p_caracteristicas TEXT,
-    p_fecha_visto DATE,
-    p_lugar_visto VARCHAR(100),
-    p_imagen BYTEA -- Cambiando a BYTEA para coincidir con la definición de la tabla
+DELIMITER //
+CREATE PROCEDURE reportar_perro_perdido(
+    IN p_id_usuario INT, -- Asumiendo que necesitamos este valor
+    IN p_celular INT, -- Asumiendo que necesitamos este valor
+    IN p_raza VARCHAR(50),
+    IN p_color VARCHAR(20),
+    IN p_tamano VARCHAR(20),
+    IN p_sexo CHAR(1),
+    IN p_caracteristicas TEXT,
+    IN p_fecha_visto DATE,
+    IN p_lugar_visto VARCHAR(100),
+    IN p_imagen BLOB -- Cambiando a BLOB para coincidir con la definición de la tabla
 )
-LANGUAGE plpgsql
-AS $$
 BEGIN
     -- Insertar el reporte del perro perdido
     INSERT INTO mascotaperdida (id_usuario, raza, color, tamaño, sexo, caracteristicas, fecha_visto, lugar_visto, celular, imagen)
     VALUES (p_id_usuario, p_raza, p_color, p_tamano, p_sexo, p_caracteristicas, p_fecha_visto, p_lugar_visto, p_celular, p_imagen);
 
     -- Devolver mensaje de éxito
-    RAISE NOTICE 'Reporte del perro perdido creado correctamente';
-END;
-$$;
+    SELECT 'Reporte del perro perdido creado correctamente' AS notice;
+END//
+DELIMITER ;
