10bd26c03205796e2240394c50d520aef2962ba9
BridgetFraire <77119331+BridgetFraire@users.noreply.github.com>
2024-04-18 20:23:31 -0600

    Update PermisosPresenter.cs

--- ITD.PerrosPerdidos.Aplication/PermisosPresenter.cs
+++ ITD.PerrosPerdidos.Aplication/PermisosPresenter.cs
@@ -8,15 +8,20 @@ namespace ITD.PerrosPerdidos.Aplication.Presenters
 {
   public class PermisosPresenter
   {
+    public ErrorResponse _errorResponse {get;set}
     private readonly ISimulacionContextRepository _repo;
     PermisosPresenter(ISimulacionContextRepository repo)
     {
+      _errorResponse = new ErrorResponse();
       _repo = repo;
     }
     public async ValueTask<PermisosResponse>Post(RequestPermisos post)
     {
       var permisos = await _repo.PermisosContext.Post(post);
-      return new PermisosResponse() {data = new PermisosData(){attributes = permisos, type = "permisos"}}
+      if (permisos.code = 201)
+        return new PermisosResponse() {data = new PermisosData(){attributes = new PermisosAttributes() {mensaje = permisos.result}, type = "permisos"}};  
+      _errorResponse.errors = new List<ErrorData>(){new ErrorData(){code = permisos.code.ToString(), detail = permisos.result, status = permisos.code, title = "todo se derrumbo"}}; 
+      return null;
     }
   }
 }
