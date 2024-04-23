12aebf9666ef53eccbf1b0afff44f7d808ed5ce2
BridgetFraire <77119331+BridgetFraire@users.noreply.github.com>
2024-04-23 10:51:05 -0600

    Update PROC_eliminar_publicaciones_por_nombre_usuario.sql

--- PROC_eliminar_publicaciones_por_nombre_usuario.sql
+++ PROC_eliminar_publicaciones_por_nombre_usuario.sql
@@ -8,17 +8,17 @@ BEGIN
     DECLARE v_id_usuario INT; -- Local variable to store the user ID based on the provided username.
 
     -- Verify if the requester is an administrator by comparing the provided ID with the records in the 'Administradores' table.
-    -- SELECT EXISTS(SELECT 1 FROM Administradores WHERE ID_Administrador = p_id_admin) INTO v_es_admin;
+    SELECT EXISTS(SELECT 1 FROM Administradores WHERE ID_Administrador = p_id_admin) INTO v_es_admin;
     
-    -- IF NOT v_es_admin THEN -- If the requester is not an administrator, an exception is thrown.
-    --    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'The user making the request does not have administrator privileges.';
-    -- END IF;
+    IF NOT v_es_admin THEN -- If the requester is not an administrator, an exception is thrown.
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '401 Unauthorized: The user making the request does not have administrator privileges.';
+    END IF;
     
     -- Search for the user ID based on the provided username in the 'USUARIO' table.
     SELECT ID_USUARIO INTO v_id_usuario FROM USUARIO WHERE USUARIO = p_nombre_usuario;
     
     IF v_id_usuario IS NULL THEN -- If the specified user is not found, an exception is thrown.
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'The specified user was not found.';
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '404 Not Found: The specified user was not found.';
     ELSE
         -- Delete all posts associated with the user. This is done by searching for all posts whose 'ID_MASCOTA'
         -- is linked to lost pets ('MASCOTAPERDIDA') which, in turn, are associated with the found 'ID_USUARIO'.
@@ -27,7 +27,13 @@ BEGIN
         );
         
         -- Emit a notice indicating that the associated posts have been successfully deleted.
-        SELECT CONCAT('All posts associated with the username ', p_nombre_usuario, ' have been deleted.') AS notice;
+        SELECT CONCAT('204 No Content: All posts associated with the username ', p_nombre_usuario, ' have been deleted.') AS notice;
     END IF;
+    
+    -- Handle any unexpected errors that may occur during the execution of the procedure.
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '500 Internal Server Error: An internal error occurred while processing the request.';
+    END;
 END //
 DELIMITER ;
