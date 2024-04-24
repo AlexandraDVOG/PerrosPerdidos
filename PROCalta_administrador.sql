ab3f4eaac43f37477299df3e1a14002e1d7fd7a3
BridgetFraire <77119331+BridgetFraire@users.noreply.github.com>
2024-04-23 19:51:50 -0600

    Create PROCalta_administrador.sql

---
+++ PROCalta_administrador.sql
@@ -0,0 +1,18 @@
+DELIMITER //
+CREATE PROCEDURE alta_administrador(
+    IN p_usuario VARCHAR(255),
+    IN p_telefono BIGINT,
+    IN p_contrasena VARCHAR(255)
+)
+BEGIN
+    -- Insertar el nuevo administrador
+    INSERT INTO administradores (usuario, telefono, contraseña)
+    VALUES (p_usuario, p_telefono, p_contrasena);
+
+    -- Obtener el ID del nuevo administrador
+    SET @v_admin_id = LAST_INSERT_ID();
+
+    -- Imprimir el ID del nuevo administrador
+    SELECT CONCAT('Nuevo administrador creado con ID: ', @v_admin_id) AS mensaje;
+END//
+DELIMITER ;
