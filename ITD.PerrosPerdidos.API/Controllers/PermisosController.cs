f0e1f2ac32cb447d3a08b3ee713665fd517f4445
BridgetFraire <77119331+BridgetFraire@users.noreply.github.com>
2024-04-28 23:08:58 -0600

    Patch y Get

--- ITD.PerrosPerdidos.API/Controllers/PermisosController.cs
+++ ITD.PerrosPerdidos.API/Controllers/PermisosController.cs
@@ -1,5 +1,83 @@
+  using ITD.PerrosPerdidos.Application.Interfaces.Presenters;
+using ITD.PerrosPerdidos.Domain.Enums;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using System.Threading.Tasks;
 
-using ITD.PerrosPerdidos.Application.Interfaces.Presenters;
+namespace ITD.PerrosPerdidos.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Consumes("application/json")]
+    [ProducesResponseType(typeof(PermisosResponse), (int)StatusHttp.badRequest)]
+    public class PermisosController : ControllerBase
+    {
+        private readonly IPermisosPresenter _permisosPresenter;
+
+        public PermisosController(IPermisosPresenter permisosPresenter)
+        {
+            _permisosPresenter = permisosPresenter;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _permisosPresenter.GetPermisoByIdAsync(id);
+            if (_permisosPresenter._error.Count > 0)
+            {
+                return BadRequest(_permisosPresenter._error);
+            }
+            return result != null ? Ok(result) : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] RequestPermisos post)
+        {
+            var result = await _permisosPresenter.Post(post);
+            if (result != null)
+                return Created("www.google.com", result);
+            return BadRequest(_permisosPresenter);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> Patch(int id, [FromBody] JsonPatchDocument<RequestPermisos> patchDoc)
+        {
+            if (patchDoc != null)
+            {
+                var permiso = await _permisosPresenter.GetPermisoByIdAsync(id);
+
+                if (permiso == null)
+                {
+                    return NotFound();
+                }
+
+                patchDoc.ApplyTo(permiso, ModelState);
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                bool result = await _permisosPresenter.UpdatePermisoAsync(permiso);
+
+                if (!result)
+                {
+                    return BadRequest("Error al actualizar el permiso");
+                }
+
+                return new ObjectResult(permiso);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
+
+
+/*using ITD.PerrosPerdidos.Application.Interfaces.Presenters;
 using ITD.PerrosPerdidos.Domain.Enums;
 using Microsoft.AspNetCore.Mvc;
 
@@ -32,3 +110,4 @@ namespace ITD.PerrosPerdidos.API.Controllers
         }
     }
 }
+*/
