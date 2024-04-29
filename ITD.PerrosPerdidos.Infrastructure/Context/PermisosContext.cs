0f69f442f25f1382b4d6b03f93dec121a3e91f4a
BridgetFraire <77119331+BridgetFraire@users.noreply.github.com>
2024-04-28 21:54:38 -0600

    PermisosContext

--- ITD.PerrosPerdidos.Infrastructure/Context/PermisosContext.cs
+++ ITD.PerrosPerdidos.Infrastructure/Context/PermisosContext.cs
@@ -3,6 +3,52 @@ using ITD.PerrosPerdidos.Application.Interfaces;
 using ITD.PerrosPerdidos.Domain.DTO.DATA;
 using ITD.PerrosPerdidos.Domain.POCOS.Context;
 using ITD.PerrosPerdidos.Infrestructura.Services;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITD.PerrosPerdidos.Infrestucture.Repostory.Context
+{
+    public class MascotasPerdidasContext : IMascotasPerdidasContext
+    {
+        private readonly BdContext _bd;
+        public MascotasPerdidasContext(BdContext bd)
+        {
+            _bd = bd;
+        }
+
+        public async Task<string> ModificarCaracteristicasPerroPerdido(int idMascota, string nuevasCaracteristicas)
+        {
+            DynamicParameters dp = new DynamicParameters();
+            dp.Add("@p_id_mascota", idMascota, DbType.Int32, ParameterDirection.Input);
+            dp.Add("@p_nuevas_caracteristicas", nuevasCaracteristicas, DbType.String, ParameterDirection.Input);
+            var result = await _bd.ExecuteStoredProcedureQuery<string>("modificar_caracteristicas_perro_perdido", dp);
+            return result.FirstOrDefault();
+        }
+
+        public async Task<string> ReportarPerroPerdido(int idUsuario, int celular, string raza, string color, string tamano, char sexo, string caracteristicas, DateTime fechaVisto, string lugarVisto, byte[] imagen)
+        {
+            DynamicParameters dp = new DynamicParameters();
+            // Aquí añades todos los parámetros necesarios para el procedimiento almacenado
+            // ...
+            var result = await _bd.ExecuteStoredProcedureQuery<string>("reportar_perro_perdido", dp);
+            return result.FirstOrDefault();
+        }
+
+        public async Task<List<MascotasPerdidasContext>> ObtenerPublicacionesRecientes()
+        {
+            var result = await _bd.ExecuteStoredProcedureQuery<MascotasPerdidasContext>("obtener_publicaciones_recientes", null);
+            return result.ToList();
+        }
+    }
+}
+
+/*
+using Dapper;
+using ITD.PerrosPerdidos.Application.Interfaces;
+using ITD.PerrosPerdidos.Domain.DTO.DATA;
+using ITD.PerrosPerdidos.Domain.POCOS.Context;
+using ITD.PerrosPerdidos.Infrestructura.Services;
 
 
 namespace ITD.PerrosPerdidos.Infrestucture.Repostory.Context
@@ -36,3 +82,4 @@ namespace ITD.PerrosPerdidos.Infrestucture.Repostory.Context
         }
     }
 }
+*/
